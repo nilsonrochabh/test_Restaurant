a3ef596e67c1f27897c4821d2d52f4ebf9b8e14f
nilsonrochabh <nilson.f.rocha@gmail.com>
2025-12-17 08:32:48 -0300

    feat: login and initial screens

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path 
-from myapp import views
+from .views import home
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-    path('', views.mysite, name='mysite'), 
+    path('', home, name='home'), 
+    path('login/', LoginView.as_view(template_name='auth/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from .models import Restaurant
 
-# Create your views here.
-def mysite(request):
-    return render(request, 'index.html')
+def home(request):
+    # Fetch all restaurant objects from the database
+    restaurants = Restaurant.objects.all()
+    
+    # Render the 'home.html' template with the list of restaurants
+    return render(request, 'home.html', {'restaurants': restaurants})
